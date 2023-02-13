dfae665404ed25e6d2f4653fd7caaae8c1e5cdb6
LLarbi <larbi_lachgare@hotmail.fr>
2023-02-13 15:16:22 +0100

    add event entity controller service repository

---
+++ src/main/java/com/pie/planingapispring/controller/EventController.java
@@ -0,0 +1,9 @@
+package com.pie.planingapispring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/events")
+public class EventController {
+}
