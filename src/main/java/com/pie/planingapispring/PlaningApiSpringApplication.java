8114cde7cf1f6238ec251ca6e70d12a6752b4544
Jat15 <jinan15@hotmail.com>
2023-02-13 14:15:19 +0100

    Init Project PLANING

---
+++ src/main/java/com/pie/planingapispring/PlaningApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.pie.planingapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaningApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaningApiSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/pie/planingapispring/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.pie.planingapispring.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
