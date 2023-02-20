5bd225faefee1f44f4f1a20d0eb3e4ed103e0bde
LLarbi <larbi_lachgare@hotmail.fr>
2023-02-20 19:47:53 +0100

    add event Dto

---
+++ src/main/java/com/pie/planingapispring/dto/EventDto.java
@@ -0,0 +1,54 @@
+package com.pie.planingapispring.dto;
+
+import java.time.LocalDateTime;
+
+public class EventDto {
+    private Integer id;
+    private String title;
+    private String message;
+    private LocalDateTime start_date;
+    private LocalDateTime end_date;
+
+    public EventDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDateTime start_date) {
+        this.start_date = start_date;
+    }
+
+    public LocalDateTime getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDateTime end_date) {
+        this.end_date = end_date;
+    }
+}
