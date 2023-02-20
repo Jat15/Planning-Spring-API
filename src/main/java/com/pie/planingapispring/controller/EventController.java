b35971dac3d6d6e3e3febdaaf6d194c1bbb5e90f
LLarbi <larbi_lachgare@hotmail.fr>
2023-02-20 19:49:21 +0100

    add method get event by Id in controller

--- src/main/java/com/pie/planingapispring/controller/EventController.java
+++ src/main/java/com/pie/planingapispring/controller/EventController.java
@@ -1,9 +1,28 @@
 package com.pie.planingapispring.controller;
 
+import com.pie.planingapispring.dto.EventDto;
+import com.pie.planingapispring.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path="/events")
+@RequestMapping(path="/api/events")
 public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventDto> fetchById(@PathVariable("id") Integer id) {
+        EventDto event = eventService.findById(id);
+
+        if(event == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(event);
+    }
 }
