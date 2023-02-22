75eb9d59fde0e92972de58592ab904e3b27c49c5
Jat15 <jinan15@hotmail.com>
2023-02-22 23:33:37 +0100

    [Entity] Update Planning
    - Add name

--- src/main/java/com/pie/planingapispring/entity/Planning.java
+++ src/main/java/com/pie/planingapispring/entity/Planning.java
@@ -1,6 +1,7 @@
 package com.pie.planingapispring.entity;
 
 import jakarta.persistence.*;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,12 +9,11 @@ import java.util.List;
 @Entity
 @Table(name = "plannings")
 public class Planning {
-
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name = "planning_id")
     private Integer id;
-
+    private String name;
     @OneToMany(mappedBy = "planning")
     private List<Event> events = new ArrayList<>();
 
@@ -37,5 +37,21 @@ public class Planning {
 
     public void setEvents(List<Event> events) {
         this.events = events;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<UserPlanning> getUsersPlannings() {
+        return usersPlannings;
+    }
+
+    public void setUsersPlannings(List<UserPlanning> usersPlannings) {
+        this.usersPlannings = usersPlannings;
     }
 }
