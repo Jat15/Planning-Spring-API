f39751d1f64c683c1b46e3fa37d270a27c91db00
teeboude <marceldu03@gmail.com>
2023-02-23 16:00:14 +0100

    [USER] add method toEntity on mapper

--- src/main/java/com/pie/planingapispring/mapper/UserMapper.java
+++ src/main/java/com/pie/planingapispring/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.pie.planingapispring.mapper;
 
+import com.pie.planingapispring.dto.CreateUserDto;
 import com.pie.planingapispring.dto.UserDto;
 import com.pie.planingapispring.entity.User;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,25 @@ public class UserMapper {
         userDto.setAvatar(user.getAvatar());
 
         return userDto;
+    }
+
+    public static User fromCreateUserDtoToEntity(CreateUserDto dto) {
+        User user = new User();
+
+        user.setPseudo(dto.getPseudo());
+        user.setEmail(dto.getEmail());
+        user.setLastName(dto.getLastname());
+        user.setFirstName(dto.getFirstname());
+        user.setAvatar(dto.getAvatar());
+        user.setBirthdate(dto.getBirthdate());
+        user.setPhone(dto.getPhone());
+        user.setPassword(dto.getPassword());
+
+        user.setStreet(dto.getStreet());
+        user.setCity(dto.getCity());
+        user.setCountry(dto.getCountry());
+        user.setZip(dto.getZip());
+
+        return user;
     }
 }
