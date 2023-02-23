604769f88119631036cdd82ae568327982e50472
Jat15 <jinan15@hotmail.com>
2023-02-23 10:34:48 +0100

    [Mapper] Update UserPlanning
    - PlanningDto > PlanningRefactorDto

--- src/main/java/com/pie/planingapispring/mapper/UserPlanningMapper.java
+++ src/main/java/com/pie/planingapispring/mapper/UserPlanningMapper.java
@@ -1,13 +1,13 @@
 package com.pie.planingapispring.mapper;
 
-import com.pie.planingapispring.dto.PlanningDto;
+import com.pie.planingapispring.dto.PlanningRefactorDto;
 import com.pie.planingapispring.entity.UserPlanning;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserPlanningMapper {
-    public static PlanningDto userPlanningtoPlanningDto(UserPlanning original) {
-        PlanningDto resultDto = new PlanningDto();
+    public static PlanningRefactorDto userPlanningtoPlanningDto(UserPlanning original) {
+        PlanningRefactorDto resultDto = new PlanningRefactorDto();
 
         resultDto.setId(original.getId().getPlanningId());
         resultDto.setName(original.getPlanning().getName());
