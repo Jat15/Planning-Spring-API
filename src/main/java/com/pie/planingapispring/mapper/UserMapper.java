030208b9953b267ec2cbd7894b4b7f6d478afb9b
Jat15 <jinan15@hotmail.com>
2023-02-26 01:22:49 +0100

    [Mapper] Update User
    - Add mapper for patch

--- src/main/java/com/pie/planingapispring/mapper/UserMapper.java
+++ src/main/java/com/pie/planingapispring/mapper/UserMapper.java
@@ -1,12 +1,14 @@
 package com.pie.planingapispring.mapper;
 
 import com.pie.planingapispring.dto.CreateUserDto;
+import com.pie.planingapispring.dto.ModifyUserDto;
 import com.pie.planingapispring.dto.UserDto;
 import com.pie.planingapispring.entity.Role;
 import com.pie.planingapispring.entity.User;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
@@ -47,5 +49,48 @@ public class UserMapper {
 
         user.setCreatedDate(LocalDateTime.now());
         return user;
+    }
+
+    public static User userModifytoUser(ModifyUserDto dto, User user) {
+
+        if (dto.getPseudo().isPresent()) {
+            user.setPseudo(dto.getPseudo().get());
+        }
+        if (dto.getEmail().isPresent()) {
+            user.setEmail(dto.getEmail().get());
+        }
+        if (dto.getLastname().isPresent()) {
+            user.setLastName(dto.getLastname().get());
+        }
+        if (dto.getFirstname().isPresent()) {
+            user.setFirstName(dto.getFirstname().get());
+        }
+        if (dto.getAvatar().isPresent()) {
+            user.setAvatar(dto.getAvatar().get());
+        }
+        if (dto.getBirthdate().isPresent()) {
+            user.setBirthdate(dto.getBirthdate().get());
+        }
+        if (dto.getPhone().isPresent()) {
+            user.setPhone(dto.getPhone().get());
+        }
+        if (dto.getStreet().isPresent()) {
+            user.setStreet(dto.getStreet().get());
+        }
+        if (dto.getCity().isPresent()) {
+            user.setCity(dto.getCity().get());
+        }
+        if (dto.getCountry().isPresent()) {
+            user.setCountry(dto.getCountry().get());
+        }
+        if (dto.getZip().isPresent()) {
+            user.setZip(dto.getZip().get());
+        }
+        if (dto.getPassword().isPresent()) {
+            String hashPassword = BCrypt.hashpw(dto.getPassword().get(), BCrypt.gensalt());
+            user.setPassword(hashPassword);
+        }
+        return user;
+
     }
 }
